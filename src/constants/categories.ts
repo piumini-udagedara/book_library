12a53f08233929db57c681dfcff3d2aef3a49461
piumini sugandhika <piuminiu@gmail.com>
2026-01-11 23:53:21 +0530

    chages

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -2,16 +2,19 @@
  * Available book categories
  */
 export const CATEGORIES = [
-  'All',
-  'Fiction',
-  'Non-Fiction',
-  'Mystery',
-  'Romance',
-  'Sci-Fi',
-  'Fantasy',
-  'Dystopian',
-  'Biography',
-  'History',
+  "All",
+  "Fiction",
+  "Non-Fiction",
+  "Mystery",
+  "Romance",
+  "Sci-Fi",
+  "Fantasy",
+  "Dystopian",
+  "Biography",
+  "History",
+  "Self-Help",
+  "Leadership",
+  "Biography",
 ] as const;
 
-export type Category = typeof CATEGORIES[number];
+export type Category = (typeof CATEGORIES)[number];
